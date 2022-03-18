d44ab34cc114808bccff90bdeb6ff0316e2f0bc6
an-lee <an.lee.work@gmail.com>
2022-03-18 15:33:44 +0800

    initial

---
+++ lib/trident_assistant.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "trident_assistant/version"
+
+module TridentAssistant
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/trident_assistant/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TridentAssistant
+  VERSION = "0.1.0"
+end
