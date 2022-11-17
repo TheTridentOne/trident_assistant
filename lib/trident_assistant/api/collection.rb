1db14081fe107c4afdb487d2944c881b24169d72
an-lee <an.lee.work@gmail.com>
2022-11-17 13:37:35 +0800

    fix collection split limit

--- lib/trident_assistant/api/collection.rb
+++ lib/trident_assistant/api/collection.rb
@@ -35,7 +35,7 @@ module TridentAssistant
               symbol: kwargs[:symbol],
               description: kwargs[:description],
               external_url: kwargs[:external_url],
-              split: kwargs[:split].to_f.round(2),
+              split: kwargs[:split].to_f.round(4),
               icon_base64: kwargs[:icon_base64],
               icon_url: kwargs[:icon_url]
             }
